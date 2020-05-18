e13bd906aa0c4b31197bacf0b2abf95bdd278c83
Roberto Whittaker-Troncoso <robertowhittaker@theambassadors.com>
2020-05-18 18:21:53 +0100

    Initial Commit

---
+++ PropertyAnalysisDB/Integration/Tables/PropertyPricesUpdate.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [Integration].[PropertyPricesUpdate]
+(
+	  [tuid] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY
+	, price INT
+	, transferDate DATETIME
+	, postcode VARCHAR(20)
+	, propertyType CHAR(1)
+	, oldNew CHAR(1)
+	, duration CHAR(1)
+	, paon NVARCHAR(100)
+	, saon NVARCHAR(100)
+	, street NVARCHAR(100)
+	, locality NVARCHAR(100)
+	, town NVARCHAR(100)
+	, district NVARCHAR(100)
+	, county NVARCHAR(100)
+	, ppdCategory CHAR(1)
+	, recordStatus CHAR(1)
+)
